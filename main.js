e9d1acca3614c4a73bfbe1c479b7da3b49166096
Fluffhead88 <thigpen.zach@gmail.com>
2018-07-05 16:55:37 -0400

    getting results data into html templates

--- main.js
+++ main.js
@@ -2,12 +2,22 @@
   'use strict';
 
   $.ajax({
-    url: 'https://api.etsy.com/v2/listings/active.js?api_key=l1d8vqh6foq3xp6p2770bp3i&keywords=yarn&includes=Images,Shop&limit=25',
+    url: 'https://api.etsy.com/v2/listings/active.js?api_key=l1d8vqh6foq3xp6p2770bp3i&keywords=guitarstrap&includes=Images,Shop&limit=25',
     type: 'GET',
     dataType: 'jsonp',
-    success: function(res) {
-      console.log(res);
-    }
+    success: getResults
   });
+
+  function getResults(res) {
+    console.log(res);
+
+    var source = document.getElementById('main-template').innerHTML;
+    var template = Handlebars.compile(source);
+
+    var context = res;
+    var html = template(context);
+
+    $('.template-data').html(html);
+  }
 
 })();
