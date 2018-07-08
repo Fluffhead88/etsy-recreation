740d0627d714c1d6209b8d75e929be40f05f4ac2
Fluffhead88 <thigpen.zach@gmail.com>
2018-07-08 15:04:18 -0400

    added html for sidebar

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 (function() {
   'use strict';
-
+  // ajax call to etsy API to retrieve info for items
   $.ajax({
     url: 'https://api.etsy.com/v2/listings/active.js?api_key=l1d8vqh6foq3xp6p2770bp3i&keywords=guitarstrap&includes=Images,Shop&limit=25',
     type: 'GET',
     dataType: 'jsonp',
     success: getResults
   });
-
+  // creates function to be called on successful ajax call
   function getResults(res) {
     console.log(res);
-
+    // creats template for handlebars to insert into html
     var source = document.getElementById('main-template').innerHTML;
     var template = Handlebars.compile(source);
 
     var context = res;
     var html = template(context);
-
+    // names info template-data to be linked in html
     $('.template-data').html(html);
   }
 
